0a7fda40ec5967da6e4976f87bc37d8bd8f5181f
johnjung <john@johnjung.us>
2023-03-27 14:56:00 -0500

    Update view and template for object page.

--- guerrilla_television/views.py
+++ guerrilla_television/views.py
@@ -1,17 +1,31 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from guerrilla_television.models import GuerrillaTelevisionItem
 
 def object(request, id):
     # look up the GuerrillaTelevisionItem object, use id.
     # this works...
 
-    vimeo_id = 'vimeo id'
-    title = 'title'
-    main_contributors = 'main contributors'
-    date_notation = 'date notation'
-    video_tape_format = 'video tape format'
-    location_of_production = 'location of production'
-    collection = 'collection'
+    object = GuerrillaTelevisionItem.objects.get(pk=id)
+
+    vimeo_id = object.vimeo_id
+    title = object.title
+
+    creator = ' ; '.join([p.value for p in object.producers.all()])
+    if object.main_credits:
+        creator = creator + '; ' + object.main_credits
+    if object.additional_credits:
+        creator = creator + '; ' + object.additional_credits
+
+    production_dates = ' ; '.join([d.value for d in object.production_dates.all()])
+    
+    video_tape_format = object.video_tape_format.value
+    genres = ' ; '.join([g.value for g in object.genres.all()])
+    excerpt = object.excerpt
+    categories = ' ; '.join([c.value for c in object.categories.all()])
+    location_of_production = object.location_of_production
+
+    collections = ' ; '.join([c.value for c in object.collections.all()])
      
     return render(
         request, 
@@ -20,10 +34,13 @@ def object(request, id):
             'id': id,
             'vimeo_id': vimeo_id,
             'title': title,
-            'main_contributors': main_contributors,
-            'date_notation': date_notation,
+            'creator': creator,
+            'production_dates': production_dates,
             'video_tape_format': video_tape_format,
+            'genres': genres,
+            'excerpt': excerpt,
+            'categories': categories,
             'location_of_production': location_of_production,
-            'collection': collection
+            'collections': collections
         }
     )
